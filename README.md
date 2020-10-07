6f1cebeaef515d05b0491bed9a07a1f8ca9c09d6
Thalia <thalia.rowan@gmail.com>
2020-10-07 00:50:52 -0300

    starting links

---
+++ README.md
@@ -0,0 +1,37 @@
+
+    
+# Thalia
+
+
+## License
+license here 
+    
+ it is about me
+    
+## Table of Contents
+
+* [Intsallation](Installation)
+* Usage
+* Contributing
+* Tests
+* [QuestionsMy](https://github.com/ThaliaRowan/readme_generator)
+    
+## Installation
+just download
+    
+    
+## Usage
+    
+by using it
+    
+## Contributing
+no
+
+## Tests
+no
+## Questions
+
+thaliagmail
+thaliaRowan
+    
+    
--- index.js
+++ index.js
@@ -1,3 +1,110 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
+
+const writeFileAsynch = util.promisify(fs.writeFile);
+
+ function promptReadme() {
+    return  inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project?"
+        },
+        {
+            type:"input",
+            message: "Add your project decription",
+            name: "description"
+        },
+        {
+            type: "input",
+            message: "Add project installation instructions",
+            name: "installation",
+            
+        },
+        {
+            type: "input",
+            message: "How to use the your application",
+            name: "usage"
+        },
+        {
+            type: "input",
+            message: "Add your contributer's",
+            name: "collab"
+        },
+        {
+            type: "input",
+            message: "Add test example",
+            name: "test"
+    
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "Add your email"
+        },
+        {
+            type: "input",
+            name: "myGit",
+            message: "What is yout github"
+        }
+    ])
+}
+
+
+
+
+function generate(answers){
+    return `
+    
+## ${answers.name}
+
+
+# License
+license here 
+    
+ ${answers.description}
+    
+# Table of Contents
+
+* Intsallation
+* Usage
+* Contributing
+* Tests
+* QuestionsMy
+    
+# Installation
+${answers.installation}
+    
+    
+# Usage
+    
+${answers.usage}
+    
+# Contributing
+${answers.collab}
+
+# Tests
+${answers.test}
+# Questions
+
+${answers.email}
+${answers.myGit}
+    
+    `;
+}
+
+async function init(){
+    try {
+        const answers = await promptReadme();
+
+        const generated = generate(answers);
+        await writeFileAsynch('README.md', generated);
+
+        console.log('Success')
+    } catch(err){
+        console.log(err)
+    }
+}
+
+init();
